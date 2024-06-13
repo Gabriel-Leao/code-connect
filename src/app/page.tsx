6a041637e210313f3e243a88019de60e5e5710ff
Gabriel-Leao <gabriel.leao2507@gmail.com>
2024-06-13 13:08:56 -0300

    feat: pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,28 @@ import Link from 'next/link'
 import prisma from '../../prisma/prisma'
 
 const getPosts = async (page: number) => {
-  console.log(page)
   try {
+    const take = 6
+    const skip = (page - 1) * take
+    const prev = page > 1 ? page - 1 : false
+    const totalPage = Math.ceil((await prisma.post.count()) / take)
+    const next = page < totalPage ? page + 1 : null
     const posts = await prisma.post.findMany({
-      include: { author: true }
+      take,
+      skip,
+      include: { author: true },
+      orderBy: { created_at: 'desc' }
     })
     logger.info('Posts encontrados com sucesso')
-    return { posts, next: null, prev: null }
+    return { posts, next, prev }
   } catch (error) {
     logger.error(error)
     return { posts: [], next: null, prev: null }
   }
 }
 
-const Home = async ({ searchParams }: { searchParams: { page: number } }) => {
-  const currentPage = searchParams?.page || 1
+const Home = async ({ searchParams }: { searchParams: { page: string } }) => {
+  const currentPage = parseInt(searchParams?.page) || 1
   const { posts, prev, next } = await getPosts(currentPage)
   return (
     <main>
